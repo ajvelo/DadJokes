1bd9de34061f02fabe7bcf0e7ec8dd0995910b68
ajvelo <>
2019-12-12 17:16:27 +0000

    Add custom UI

--- DadJokes/DadJokes/ContentView.swift
+++ DadJokes/DadJokes/ContentView.swift
@@ -17,26 +17,29 @@ struct ContentView: View {
     @State private var showingAddJoke = false
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(jokes, id: \.setup) { joke in
-                    NavigationLink(destination:
-                    Text(joke.punchline)) {
-                        EmojiView(for: joke.rating)
-                        Text(joke.setup)
+        ZStack(alignment: .top) {
+            LinearGradient(gradient: Gradient(colors: [Color("Start"), Color("Middle"), Color("End")]), startPoint: .top, endPoint: .bottom)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(jokes, id: \.setup) { joke in
+                        JokeCard(joke: joke)
                     }
                 }
-            .onDelete(perform: removeJokes(at:))
             }
-            .navigationBarTitle("All Groan Up")
-            .navigationBarItems(leading: EditButton(), trailing:
-                Button("Add") {
-                    self.showingAddJoke.toggle()
-                }
-            )
-            .sheet(isPresented: $showingAddJoke) {
-                AddView().environment(\.managedObjectContext, self.moc)
+            
+            Button("Add Joke") {
+                self.showingAddJoke.toggle()
             }
+            .padding()
+            .background(Color.black.opacity(0.5))
+            .clipShape(Capsule())
+            .foregroundColor(.white)
+            .offset(y: 50)
+        }
+        .edgesIgnoringSafeArea(.all)
+        .sheet(isPresented: $showingAddJoke) {
+            AddView().environment(\.managedObjectContext, self.moc)
         }
     }
     
