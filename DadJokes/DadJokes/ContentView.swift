a98385f89b6c436ee85e2027343be6dab5025502
ajvelo <>
2019-12-12 14:09:05 +0000

    Create dad jokes struct

--- DadJokes/DadJokes/ContentView.swift
+++ DadJokes/DadJokes/ContentView.swift
@@ -8,9 +8,25 @@
 
 import SwiftUI
 
+struct Joke {
+    var setup: String
+    var punchline: String
+    var rating: String
+}
+
 struct ContentView: View {
+    let jokes = [
+        Joke(setup: "What's a cow's favorite place?", punchline: "A mooseum", rating: "Silence"),
+        Joke(setup: "What's brown and sticky?", punchline: "A stick", rating: "Sigh"),
+        Joke(setup: "What's orange and sounds like a carrot?", punchline: "A carrot", rating: "Smirk")
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            ForEach(jokes, id: \.setup) { joke in
+                Text(joke.setup)
+            }
+        }
     }
 }
 
